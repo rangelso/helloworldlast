26b2a3b402099f34c41b3d1f792f20d822f62f5b
Lenovo <Lenovo@192.168.30.3>
2017-05-01 23:25:27 +0200

    third check in

---
+++ helloworldlast/src/helloworldlast.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : helloworldlast.cpp
+// Author      : Tomek
+// Version     :
+// Copyright   : Moja druga trzecia w Eclipsie
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
